ec39f89a06e72d13089772dbd5f9038a5b06ac61
YuKiRoKi <69679451+YuKiRoKi@users.noreply.github.com>
2025-12-09 11:24:06 +0800

    Modified some output fields.

--- disk2bus.sh
+++ disk2bus.sh
@@ -1,8 +1,9 @@
+
 #!/usr/bin/env bash
 set -euo pipefail
 
-printf "%-12s %-10s %-10s %-8s %s\n" "DISK" "BUS" "BUS_DIR" "SIZE" "MODEL"
-printf "%-12s %-10s %-10s %-8s %s\n" "----" "----" "-------" "----" "-----"
+printf "%-12s %-10s %-10s %-8s %s\n" "DISK" "INTERFACE" "BUS" "SIZE" "MODEL"
+printf "%-12s %-10s %-10s %-8s %s\n" "----" "---------" "---" "----" "-----"
 
 mapfile -t DISKS < <(lsblk -dn -o NAME,TYPE | awk '$2=="disk"{print "/dev/"$1}')
 
@@ -10,13 +11,13 @@ for dev in "${DISKS[@]}"; do
   name="${dev#/dev/}"
   tran="$(lsblk -dn -o TRAN "$dev" 2>/dev/null | head -n1)"
   case "$tran" in
-    sata|sas) bus="sata/sas" ;;
-    usb)      bus="usb" ;;
-    nvme)     bus="nvme" ;;
-    *)        bus="${tran:-unknown}" ;;
+    sata|sas) inf="sata/sas" ;;
+    usb)      inf="usb" ;;
+    nvme)     inf="nvme" ;;
+    *)        inf="${tran:-unknown}" ;;
   esac
   sys_path="$(readlink -f "/sys/block/$name")"
-  bus_dir="$(
+  bus="$(
     awk -v p="$sys_path" 'BEGIN{
       n=split(p, a, "/");
       for(i=n;i>0;i--){
@@ -38,5 +39,5 @@ for dev in "${DISKS[@]}"; do
   model="${model:-unknown}"
 
   # outopt result
-  printf "%-12s %-10s %-10s %-8s %s\n" "$dev" "$bus" "$bus_dir" "$size" "$model"
+  printf "%-12s %-10s %-10s %-8s %s\n" "$dev" "$inf" "$bus" "$size" "$model"
 done
